11086bd99c15f87cf76b5ba9f05ef4aec6ce0f40
Arpan Abhishek <arpanforbusiness@gmail.com>
2025-01-05 00:08:46 +0530

    remove unnecessary context/sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -9,17 +9,12 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
-import { useLogContext } from "@/contexts/log-context";
-import { format } from "date-fns";
 import { Button } from "@/components/ui/button";
 
 export function AppSidebar() {
   const { setOpenMobile } = useSidebar();
-  const { logs, setSelectedDate } = useLogContext();
-  const uniqueDates = [...new Set(logs.map((log) => log.date))].reverse();
 
-  const handleButtonClick = (date: string) => {
-    setSelectedDate(date);
+  const handleButtonClick = () => {
     setOpenMobile(false);
   };
 
@@ -30,35 +25,17 @@ export function AppSidebar() {
           <SidebarGroupLabel>Previous logs</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {!uniqueDates.includes(format(new Date(), "PP")) && (
-                <SidebarMenuItem>
-                  <SidebarMenuButton asChild>
-                    <Button
-                      variant="ghost"
-                      className="flex justify-start items-start"
-                      onClick={() =>
-                        handleButtonClick(format(new Date(), "PP"))
-                      }
-                    >
-                      Today
-                    </Button>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              )}
-              {uniqueDates.length > 0 &&
-                uniqueDates.map((date) => (
-                  <SidebarMenuItem key={date}>
-                    <SidebarMenuButton asChild>
-                      <Button
-                        variant="ghost"
-                        className="flex justify-start items-start"
-                        onClick={() => handleButtonClick(date)}
-                      >
-                        {date === format(new Date(), "PP") ? "Today" : date}
-                      </Button>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Button
+                    variant="ghost"
+                    className="flex justify-start items-start"
+                    onClick={() => handleButtonClick()}
+                  >
+                    Today
+                  </Button>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
